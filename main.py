f634ac23402b8006ae3deefaaba44b80e68e9acd
Ghiroz <runozo@gmail.com>
2017-10-26 20:25:00 +0200

    getting the commit numbers

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import subprocess
-ret = subprocess.check_output(["git", "log"])
-print("UEEE: %s" % ret)
+
+def get_commits():
+    ret = subprocess.check_output(["git", "log"])
+    commits = []
+    for row in ret.split(b'\n'):
+        if row.startswith(b'commit'):
+            commits.append(row.split()[1])
+    return commits
+
+def get_diffs(comm1, comm2):
+    ret = subprocess.check_output(["git", "diff", comm1, comm2])
+    return ret
+
+
+if __name__ == "__main__":
+    print("UEEE: %s" % get_commits())
