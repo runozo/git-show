37dc446515a0e82feb8d33fe5ff0efd24af640f5
Ghiroz <runozo@gmail.com>
2017-10-26 20:43:07 +0200

    getting the diffs

--- main.py
+++ main.py
@@ -10,8 +10,19 @@ def get_commits():
 
 def get_diffs(comm1, comm2):
     ret = subprocess.check_output(["git", "diff", comm1, comm2])
-    return ret
+    return ret.split(b'\n')
 
 
 if __name__ == "__main__":
-    print("UEEE: %s" % get_commits())
+    commits = get_commits()
+    if len(commits) < 2:
+        print("you don't have enough commits")
+    else:
+        first_comm, second_comm = commits[0], commits[1]
+        print("DIFFS: %s" % get_diffs(first_comm, second_comm))
+        for comm in commits[2:]:
+            first_comm = second_comm
+            second_comm = comm
+            print("DIFFS: %s" % get_diffs(first_comm, second_comm))
+
+        
